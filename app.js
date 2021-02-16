4317591807d08be478bc090b53c4215cbcca4f88
kapil <kapil.karda@gmail.com>
2021-02-16 17:19:12 +0530

    interview

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ async function Test() {
     useFindAndModify: false,
     autoIndex: false, // Don't build indexes
     poolSize: 500, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    serverSelectionTimeoutMS: 50000, // Keep trying to send operations for 5 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     family: 4 // Use IPv4, skip trying IPv6
   };
