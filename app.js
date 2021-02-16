3f64de70683fe2bc5c354b00256e9f17cea134a3
kapil <kapil.karda@gmail.com>
2021-02-16 17:09:16 +0530

    interview

--- app.js
+++ app.js
@@ -69,17 +69,17 @@ async function Test() {
 // test
 // setTimeout(function() {
 
-  const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    autoIndex: false, // Don't build indexes
-    poolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-  };
+  // const options = {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   useCreateIndex: true,
+  //   useFindAndModify: false,
+  //   autoIndex: false, // Don't build indexes
+  //   poolSize: 10, // Maintain up to 10 socket connections
+  //   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  //   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  //   family: 4 // Use IPv4, skip trying IPv6
+  // };
 
   // {
   //   useNewUrlParser: true,
@@ -88,11 +88,28 @@ async function Test() {
   //   useCreateIndex: true,
   //   connectTimeoutMS: 1000
   // }
- mongoose
-  .connect(process.env.MONGO_URL,options)
-  .then(() => {
-    console.log("mongo connected");
-  }); 
+//  mongoose
+//   .connect(process.env.MONGO_URL,options)
+//   .then(() => {
+//     console.log("mongo connected");
+//   }); 
+
+
+const options = {
+  useNewUrlParser: true
+};
+
+mongoose.connect(process.env.MONGO_URL, options);
+
+mongoose.connection.on('error', (err)=>{
+  console.log('>> Failed to connect to MongoDB, retrying...');
+
+  setTimeout( () => {
+          mongoose.connect(mongoConnectionString, options);
+  }, 5000);
+});
+
+
 
 http.listen(process.env.PORT, () => {
   console.log("server started at ", process.env.PORT);
