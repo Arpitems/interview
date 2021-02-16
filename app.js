bc47c1809b8194097f8b51fab317daf9e826e8f9
kapil <kapil.karda@gmail.com>
2021-02-16 18:42:07 +0530

    interview

--- app.js
+++ app.js
@@ -72,12 +72,12 @@ async function Test() {
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
+    useCreateIndex: true,
+    useFindAndModify: false,
     // autoIndex: false, // Don't build indexes
     // poolSize: 500, // Maintain up to 10 socket connections
-    // serverSelectionTimeoutMS: 50000, // Keep trying to send operations for 5 seconds
-    // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    serverSelectionTimeoutMS: 50000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     // family: 4 // Use IPv4, skip trying IPv6
   };
 
